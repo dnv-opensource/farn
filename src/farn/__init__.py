712749ba7d8843258bd7814f19891627d5d795eb
Claas <claas.rostock@dnvgl.com>
2022-12-12 11:27:06 +0100

    farn/__init__.py : ensured that imported symbols get also exported

--- src/farn/__init__.py
+++ src/farn/__init__.py
@@ -1,10 +1,10 @@
 from .farn import (  # noqa: F401
-    Case,
-    Parameter,
-    create_case_folders,
-    create_case_list_files,
-    create_param_dict_files,
-    create_samples,
-    execute_command_set,
-    run_farn,
+    Case as Case,
+    Parameter as Parameter,
+    create_case_folders as create_case_folders,
+    create_case_list_files as create_case_list_files,
+    create_param_dict_files as create_param_dict_files,
+    create_samples as create_samples,
+    execute_command_set as execute_command_set,
+    run_farn as run_farn,
 )
