608a6ecc7ea43f6606002302e5b745a70689709a
Claas <claas.rostock@dnvgl.com>
2023-01-11 11:24:04 +0100

    Added missing DocStrings for public classes, methods and functions

--- src/farn/core/parameter.py
+++ src/farn/core/parameter.py
@@ -21,9 +21,23 @@ class Parameter:
 
     @property
     def type(self) -> Union[Type[float], Type[int], Type[bool], Type[str], None]:
+        """Returns the Python type of the parameter.
+
+        Returns
+        -------
+        Union[Type[float], Type[int], Type[bool], Type[str], None]
+            the Python type
+        """
         return None if self.value is None else type(self.value)
 
     @property
     def dtype(self) -> Union[np.dtype[float], np.dtype[int], np.dtype[bool], np.dtype[str], None]:  # type: ignore
+        """Returns the numpy dtype of the parameter.
+
+        Returns
+        -------
+        Union[np.dtype[float], np.dtype[int], np.dtype[bool], np.dtype[str], None]
+            the numpy dtype
+        """
         _type = self.type
         return None if _type is None else np.dtype(_type)  # type: ignore
--- src/farn/run/utils/threading.py
+++ src/farn/run/utils/threading.py
@@ -10,14 +10,31 @@ class JobQueue(Queue[Tuple[Any, Sequence[Any], Mapping[str, Any]]]):
     """JobQueue extends threading.Queue, overriding its 'put' method to accept a generic list of arguments."""
 
     def put(self, func: Any, *args: Any, **kwargs: Any):  # pyright: ignore
+        """Puts a callable object (function) in the JobQueue.
+
+        Additional positional and keyword arguments provided with *args and *kwargs
+        will be passed forward to the called function.
+
+        Parameters
+        ----------
+        func : Any
+            the callable object (function)
+        """
         super().put(item=(func, args, kwargs))
 
 
 class Worker(Thread):
-    """Worker thread executing jobs from the job queue."""
+    """Worker thread executing jobs from a job queue."""
 
     # Override constructor of Thread class
     def __init__(self, job_queue: JobQueue):
+        """Instantiates a Worker and binds it to the passed in JobQueue instance.
+
+        Parameters
+        ----------
+        job_queue : JobQueue
+            the JobQueue this Worker shall be bound to
+        """
         Thread.__init__(self)  # invoke base class constructor
         self.job_queue: JobQueue = job_queue
         self.daemon: bool = True
@@ -25,6 +42,7 @@ class Worker(Thread):
 
     # Override run() method of Thread class
     def run(self):
+        """Runs the next job from the JobQueue this Worker is bound to."""
         while True:
             try:
                 func, args, kwargs = self.job_queue.get()
