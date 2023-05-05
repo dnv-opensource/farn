afe4ce920b25b76c8f4180eddb524797335ddf83
Claas <claas.rostock@dnv.com>
2023-05-05 10:37:38 +0200

    resolved minor issues raised by pyright

--- tests/test_defaults.py
+++ tests/test_defaults.py
@@ -1,16 +1,10 @@
 # pyright: reportUnknownMemberType=false
-from copy import deepcopy
 from pathlib import Path
-from typing import Any, List, Tuple
 
-import numpy as np
 from dictIO import CppDict, DictReader
-from dictIO.utils.path import relative_path
-from numpy import ndarray
-from pandas import DataFrame
 
-from farn import create_cases, create_samples, run_farn
-from farn.core import Case, Cases, Parameter
+from farn import run_farn
+
 
 def test_distribute_default_parameters():
     # Prepare
@@ -23,10 +17,10 @@ def test_distribute_default_parameters():
     assert Path("cases").exists()
     assert Path("cases/linspaceLayer_0").exists()
     assert Path("cases/linspaceLayer_0").exists()
-   # test one output
+    # test one output
     param_dict_file = Path("cases/linspaceLayer_0/paramDict")
     param_dict: CppDict = DictReader.read(param_dict_file, comments=False)
-    
-    assert param_dict['param0'] == 0.
-    assert param_dict['param1'] == 1.
-    assert param_dict['param2'] == 2.
+
+    assert param_dict["param0"] == 0.0
+    assert param_dict["param1"] == 1.0
+    assert param_dict["param2"] == 2.0
