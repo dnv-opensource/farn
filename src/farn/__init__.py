cc912670426e52d1ae77620b76cc11e4164afe3d
Claas <claas.rostock@dnv.com>
2026-04-09 18:31:26 +0200

    src/farn/__init__.py: Adapted import statements, using `__all__` for re-export instead of `as xxx` in each import statement.

--- src/farn/__init__.py
+++ src/farn/__init__.py
@@ -1,11 +1,21 @@
 """farn package."""
 
-from .farn import (
-    run_farn as run_farn,
-    create_samples as create_samples,
-    create_cases as create_cases,
-    create_case_folders as create_case_folders,
-    create_param_dict_files as create_param_dict_files,
-    create_case_list_files as create_case_list_files,
-    execute_command_set as execute_command_set,
+from farn.farn import (
+    run_farn,
+    create_samples,
+    create_cases,
+    create_case_folders,
+    create_param_dict_files,
+    create_case_list_files,
+    execute_command_set,
 )
+
+__all__ = [
+    "create_case_folders",
+    "create_case_list_files",
+    "create_cases",
+    "create_param_dict_files",
+    "create_samples",
+    "execute_command_set",
+    "run_farn",
+]
